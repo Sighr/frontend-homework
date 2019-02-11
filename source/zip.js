cee40a076432b4d182b45ee662fc055e940e2cf2
Alex <alexsmekhunov@gmail.com>
2019-02-11 10:29:34 +0300

    Added jsdoc

--- source/zip.js
+++ source/zip.js
@@ -1,21 +1,35 @@
 'use strict'
 
+ /**
+  * Determines if empty.
+  *
+  * @param      {<type>}   obj     The object
+  * @return     {boolean}  True if empty, False otherwise.
+  */
+
 function isEmpty(obj) {
-    for(var key in obj) {
+    for(let key in obj) {
         if(obj.hasOwnProperty(key))
             return false;
     }
     return true;
 }
 
+ /**
+  * Zip array of objects into one. Repeating fields are added just once.
+  *
+  * @param      {Array}   objects  The objects
+  * @return     {<type>}  Zipped object.
+  */
+
 const zip = (...objects) =>
 {
-	var result = {};
-	for (var i = objects.length - 1; i >= 0; i--)
+	let result = {};
+	for (let i = objects.length - 1; i >= 0; i--)
 	{
 		if(isEmpty(objects[i]))
 			continue;
-		for(var field in objects[i])
+		for(let field in objects[i])
 		{
 			result[field] = objects[i][field];
 		}
