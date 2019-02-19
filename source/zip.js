9cdaeee7746f3ade550cdda52944fe44c4cd5b94
Alex <alexsmekhunov@gmail.com>
2019-02-19 21:37:45 +0300

    JSDoc repaired. Moved from for-loop to reduceRight

--- source/zip.js
+++ source/zip.js
@@ -1,37 +1,33 @@
 'use strict'
 
  /**
-  * Determines if empty.
-  *
-  * @param      {<type>}   obj     The object
-  * @return     {boolean}  True if empty, False otherwise.
-  */
-
+	* Determines if empty.
+	*
+	* @param      {object}   obj     The object
+	* @return     {boolean}  True if empty, False otherwise.
+	*/
 function isEmpty(obj) {
-    for(let key in obj) {
-        if(obj.hasOwnProperty(key)) {
-            return false;
-        }
-    }
-    return true;
+		for(let key in obj) {
+				if(obj.hasOwnProperty(key)) {
+						return false;
+				}
+		}
+		return true;
 }
 
  /**
-  * Zip array of objects into one. Repeating fields are added just once.
-  *
-  * @param      {Array}   objects  The objects
-  * @return     {<type>}  Zipped object.
-  */
-
+	* Zip array of objects into one. Repeating fields are added just once.
+	*
+	* @param      {Array}   objects  The array of the objects
+	* @return     {object}  Zipped object.
+	*/
 const zip = (...objects) => {
-	let result = {};
-	for (let i = objects.length - 1; i >= 0; i--) {
-		if(isEmpty(objects[i])) {
-			continue;
-		}
-		for(let field in objects[i]) {
-			result[field] = objects[i][field];
-		}
-	}
-	return result;
+	return objects.reduceRight((result, current) => {
+	 if(isEmpty(objects[i])) {
+		 continue;
+	 }
+	 for(let field in objects[i]) {
+		 result[field] = objects[i][field];
+	 }
+	}, {})
 }
