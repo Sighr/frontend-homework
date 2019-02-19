76d588298c59d573b2ad337431a0d8866cbe98b5
Alex <alexsmekhunov@gmail.com>
2019-02-19 21:51:37 +0300

    Repaired function

--- source/zip.js
+++ source/zip.js
@@ -7,12 +7,12 @@
 	* @return     {boolean}  True if empty, False otherwise.
 	*/
 function isEmpty(obj) {
-		for(let key in obj) {
-				if(obj.hasOwnProperty(key)) {
-						return false;
-				}
-		}
-		return true;
+	for(let key in obj) {
+			if(obj.hasOwnProperty(key)) {
+					return false;
+			}
+	}
+	return true;
 }
 
  /**
@@ -21,13 +21,26 @@ function isEmpty(obj) {
 	* @param      {Array}   objects  The array of the objects
 	* @return     {object}  Zipped object.
 	*/
+// const zip = (...objects) => {
+// 	let result = {};
+// 	for (let i = objects.length - 1; i >= 0; i--) {
+// 		if(isEmpty(objects[i])) {
+// 			continue;
+// 		}
+// 		for(let field in objects[i]) {
+// 			result[field] = objects[i][field];
+// 		}
+// 	}
+// 	return result;
+// }
 const zip = (...objects) => {
 	return objects.reduceRight((result, current) => {
-	 if(isEmpty(objects[i])) {
-		 continue;
-	 }
-	 for(let field in objects[i]) {
-		 result[field] = objects[i][field];
-	 }
+		if(isEmpty(current)) {
+			return result;
+		}
+		for(let field in current) {
+			result[field] = current[field];
+		}
+		return result;
 	}, {})
 }
